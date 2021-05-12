f9174c70b012983859d3b9e983fb9ed51f5452c1
Patrick <patver1974@gmail.com>
2021-05-12 13:48:31 +0200

    are

---
+++ app/src/main/java/be/bxl/formation/demo01/MainActivity.java
@@ -0,0 +1,21 @@
+package be.bxl.formation.demo01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String msg;
+        Log.d("test", "l'activite a ete creer");
+
+
+
+    }
+}
